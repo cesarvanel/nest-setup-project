0f0aa04bc59e7e231081c7bb4b36c40e623fc00d
cesarvanel <cesarzoleko@gmail.com>
2026-06-19 16:14:19 +0100

    [FRONT-APP] update  session strastegy

---
+++ back-app/src/common/config/app.config.ts
@@ -0,0 +1,9 @@
+import { registerAs } from '@nestjs/config';
+
+export const appConfig = registerAs('app', () => ({
+  port: parseInt(process.env.PORT ?? '4500', 10),
+  nodeEnv: process.env.NODE_ENV ?? 'development',
+  isMaintenance: process.env.MAINTENANCE === 'true',
+  isDevMode:process.env.NODE_ENV ==="development",
+  isProdMode:process.env.NODE_ENV ==="production",
+}));
